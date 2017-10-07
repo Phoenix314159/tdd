ec5976b0c2420c4dee651e1628460f1f920b9393
James Langford <derivative7117@live.com>
2017-10-07 12:32:45 -0600

    initial commit

---
+++ test/components/app_test.js
@@ -0,0 +1,15 @@
+import { renderComponent , expect } from '../test_helper';
+import App from '../../src/components/app';
+
+
+    describe('App', () => {
+        //use 'it to test a single attribute of a target
+        it('shows the correct text', () => {
+
+            //create an instance of App
+            const component = renderComponent(App);
+            //use expect to make an assertion about a target
+            expect(component).to.contain('Hello Man');
+        });
+    });
+
